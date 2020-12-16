6e138551beeac2fc217de1d0f48c7d15c1375586
george <gfa61@yahoo.com>
2020-12-16 10:54:54 +0200

    feat: add an empty MovieDetailsActivity with its menu and an up action button

--- app/src/main/java/com/example/mymovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/mymovies/MovieDetailsActivity.java
@@ -3,6 +3,9 @@ package com.example.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MovieDetailsActivity extends AppCompatActivity {
     public static final String MOVIE_INDEX = "movie_index";
@@ -11,5 +14,31 @@ public class MovieDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        if (getSupportActionBar() != null) {
+            //getSupportActionBar().setHomeButtonEnabled(true);
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_movie_details, menu);
+        return true;
+    }
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Log.w("back item id", String.valueOf(item.getItemId()));
+        // Log.w("androiditem id", String.valueOf(android.R.id.home));
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }*/
 }
